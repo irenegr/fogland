f86b27444049307d8488ba46e4d1312dc65b6fb4
irenegr <irenegr92@gmail.com>
2012-12-16 11:09:43 +0200

    Initial commit

---
+++ conkyscripts/gmail.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import sys, imaplib
+
+port = 993
+server = 'imap.gmail.com'
+
+username = 'YOURUSERNSME'
+passwd = 'YOURPASSWORD'
+
+imap_server = imaplib.IMAP4_SSL(server, port)
+try:
+	imap_server.login(username, passwd)
+except:
+	print('?? new')
+	sys.exit( 1 )
+
+typ, data = imap_server.select ('Inbox', True)
+if typ == 'OK':
+	total = int(data[0])
+	typ, data = imap_server.search (None, 'SEEN')
+	if typ == 'OK':
+		seen = len(data[0].split())
+		print('{}/{}'.format(total, total - seen))
+
+if typ != 'OK':
+	print('?? new')
+
+imap_server.logout()
---
+++ conkyscripts/weather.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#AccuWeather (r) RSS weather tool for conky
+#
+#USAGE: weather.sh <locationcode>
+#
+#(c) Michael Seiler 2007
+
+METRIC=1 #Should be 0 or 1; 0 for F, 1 for C
+
+if [ -z $1 ]; then
+    echo
+    echo "USAGE: weather.sh "EUR|GR|GR007|Athens""
+    echo
+    exit 0;
+fi
+
+curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 \
+| sed -n '/Currently:/ s/.*: \(.*\): \([-0-9]*\)\([CF]\).*/\1\ \2°\3/p'
+
+#Source = https://bbs.archlinux.org/viewtopic.php?pid=567663#p567663
