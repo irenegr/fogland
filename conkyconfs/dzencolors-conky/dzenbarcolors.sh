2d092b96f049b4daa2aae848a8b8e1a7704fbf34
Irene Gr (fog) <irenegr92@gmail.com>
2015-04-01 13:02:58 +0300

    Update dzenbarcolors.sh

--- conkyconfs/dzencolors-conky/dzenbarcolors.sh
+++ conkyconfs/dzencolors-conky/dzenbarcolors.sh
@@ -12,7 +12,7 @@ YPOS="0"
 # to use with conkyrc_dzen_full
 #LIN="22" #number of lines in conky_dzen output
 
-conky -c $RC | dzen2 -fg $FG -bg $BG -ta $ALIGN -h $HEIGHT -x $XPOS -y $YPOS -fn "$FONT" &
+conky -c $RC | dzen2 -e 'button2=;' -fg $FG -bg $BG -ta $ALIGN -h $HEIGHT -x $XPOS -y $YPOS -fn "$FONT" &
 # to use with conkyrc_dzen_full
 #conky -c $RC | dzen2 -e 'button1=togglecollapse;button2=exec:$HOME/bin/dzenbar_reload.sh;button3=exec:geany $HOME/.conky/conkyrc_dzen $HOME/bin/dzenbar.sh' -fg $FG -bg $BG -ta $ALIGN -h $HEIGHT -x $XPOS -y $YPOS -fn "$FONT" -l $LIN -u &
 exit 0
