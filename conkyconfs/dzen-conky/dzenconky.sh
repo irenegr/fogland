455f8b95bdf5ce3c8bdecdd4800f624aa0f3ea8a
Irene Gr (fog) <irenegr92@gmail.com>
2015-04-01 13:02:28 +0300

    Update dzenconky.sh

--- conkyconfs/dzen-conky/dzenconky.sh
+++ conkyconfs/dzen-conky/dzenconky.sh
@@ -9,5 +9,5 @@ FONT="-artwiz-smoothansi-*-*-normal-*-13-*-*-*-*-*-*-*"
 XPOS="0" 
 YPOS="754"
 
-exec conky -d -c $RC | dzen2 -fg $FG -bg $BG -ta $ALIGN -w $WIDTH -h $HEIGHT -x $XPOS -y $YPOS -fn $FONT &
+exec conky -d -c $RC | dzen2 -e 'button2=;' -fg $FG -bg $BG -ta $ALIGN -w $WIDTH -h $HEIGHT -x $XPOS -y $YPOS -fn $FONT &
 exit 0
